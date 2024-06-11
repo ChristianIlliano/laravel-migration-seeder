8bc4805ff41dbc4a0bee72bb8ddcccf0e707dfea
ChristianIlliano <grafica.illiano@gmail.com>
2024-06-11 17:27:08 +0200

    request date?

--- app/Http/Controllers/trainController.php
+++ app/Http/Controllers/trainController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Train;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class trainController extends Controller
 {
     public function index() {
-        $trains = Train::all();
+        $today = Carbon::today();
+
+        $trains = Train::where("departure_time","=",$today->toDateString())->get();
         // dd($trains);
         return view("home", compact("trains"));
     }
